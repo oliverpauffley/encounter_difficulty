b4f566b89d6634d9dce624cf5026dd5c7bc486ae
oliverpauffley <mrpauffley@gmail.com>
2023-12-07 12:24:11 +0000

    feat: single system

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
